2f3f4401cc981056bb2f8c2618b575a71f43aa21
mattvaz <146034846+mattvaz@users.noreply.github.com>
2024-03-01 15:25:51 -0800

    added.
    movement and obsticals, scripting.

--- Unit1PlayerControlVazquez/Assets/Scripts/PlayerController.cs
+++ Unit1PlayerControlVazquez/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+   private float speed = 20.0f;
+    private float turnspeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Move the card forward based on verical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up * turnspeed * horizontalInput * Time.deltaTime);
     }
 }
